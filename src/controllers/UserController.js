40d208cc0e7a35c45e11dcb0623814e6fa18796b
cleisonsantos <css.cleison@outlook.com>
2021-02-07 11:36:46 -0300

    Adicionado a criptografia de senha.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,13 +1,24 @@
 const User = require("../models/User");
 
+const bcrypt = require("bcrypt")
+
 module.exports = {
+    async index(req, res) {
+        const users = await User.findAll()
+
+        return res.json(users)
+    },
+
     async store(req, res) {
         try {
-            const { name, email, company, office, password } = req.body
+            let { name, email, company, office, password } = req.body
 
-            const user = await User.create({ name, email, company, office, isAdmin: true, password })
+            const saltRounds = 10
+            password = await bcrypt.hash(password, saltRounds)
+            
+            const user = await User.create({ name, email, company, office, isAdmin: true, password:password })
 
-            return res.json(user);
+            return res.json(user)
         } catch (err) {
             return(console.log(err))
         }
